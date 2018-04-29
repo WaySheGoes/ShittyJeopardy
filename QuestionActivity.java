21209ee586d3d1ed7c485c83af203d1f822309e4
Masterblazer <38576454+Masterblazer@users.noreply.github.com>
2018-04-29 13:51:55 -0400

    getquestions method

--- QuestionActivity.java
+++ QuestionActivity.java
@@ -1,7 +1,10 @@
 package edu.oswego.jeopardy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -9,6 +12,12 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
-
+    
+    }
+    
+    public ArrayList<Question> GetQestions(){
+        Intent intent = getIntent();
+        ArrayList<Question> q = intent.getParcelableArrayListExtra("questionsInGame");
+        return q;
     }
 }
